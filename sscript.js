599b3bbfff005dd0820f056ef611893766cd2b25
Kristofer <137994821+Krist7fer@users.noreply.github.com>
2025-12-22 16:32:45 +0200

    script.js
    Minu JavaScript

---
+++ sscript.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('main section');
+    const navButtons = document.querySelectorAll('aside button');
+
+    let currentIndex = 0;
+    sections[currentIndex].classList.add('active');
+    navButtons[currentIndex].classList.add('active');
+
+    function showSection(index) {
+        if (index < 0) index = 0;
+        if (index >= sections.length) index = sections.length - 1;
+
+        sections.forEach((sec, i) => sec.classList.toggle('active', i === index));
+        navButtons.forEach((btn, i) => btn.classList.toggle('active', i === index));
+        currentIndex = index;
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+
+    navButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const index = parseInt(btn.dataset.section);
+            showSection(index);
+        });
+    });
+
+    sections.forEach((section, idx) => {
+        const btnContainer = document.createElement('div');
+        btnContainer.classList.add('section-btns');
+
+        if (idx !== 0) {
+            const goUp = document.createElement('button');
+            goUp.innerHTML = '<i class="fas fa-arrow-up"></i>ÜLES';
+            goUp.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
+            btnContainer.appendChild(goUp);
+        }
+
+        if (idx === 0) {
+            const contactButton = document.createElement('button');
+            contactButton.innerHTML = 'KONTAKT';
+            contactButton.addEventListener('click', () => showSection(5));
+            btnContainer.appendChild(contactButton);
+        }
+
+        if (idx === 4) {
+            const backButton = document.createElement('button');
+            backButton.innerHTML = 'TAGASI';
+            backButton.addEventListener('click', () => showSection(0));
+            btnContainer.appendChild(backButton);
+        }
+
+        if (idx < sections.length - 1) {
+            const next = document.createElement('button');
+            next.innerHTML = 'JÄRGMINE<i class="fas fa-arrow-right"></i>';
+            next.addEventListener('click', () => showSection(idx + 1));
+            btnContainer.appendChild(next);
+        }
+
+        section.appendChild(btnContainer);
+    });
+});
