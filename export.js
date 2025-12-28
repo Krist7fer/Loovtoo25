a40e94a39e04ad0d4728f711103f0f19e3bb337d
Kristofer <137994821+Krist7fer@users.noreply.github.com>
2025-12-28 17:25:34 +0200

    Delete export.js

--- export.js
+++
@@ -1,9 +0,0 @@
-import { kv } from '@vercel/kv';
-
-export default async function handler(req, res) {
-    const keys = await kv.keys('entry:*');
-    const data = await Promise.all(keys.map(key => kv.get(key)));
-
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(data, null, 2));
-}
